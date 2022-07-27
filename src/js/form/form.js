32d868067abf082096f5702708b6df9773c45417
AButsai <butsai@ukr.net>
2022-07-27 17:26:14 +0300

    commit

--- src/js/form/form.js
+++ src/js/form/form.js
@@ -5,14 +5,25 @@ const { callbackForm, callbackWorning, emailInput, overlay, body } = refs;
 
 const handleSubmit = e => {
   e.preventDefault();
+
+  const formData = new FormData(callbackForm);
+  fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => {
+      window.addEventListener('keydown', modalCloseEsc);
+      overlay.classList.remove('visually-hidden');
+      body.classList.add('isOpenModal');
+    })
+    .catch(error => alert(error));
+
   const value = emailInput.value;
   if (value.trim() === '') {
     callbackWorning.classList.remove('visually-hidden');
   } else {
     callbackWorning.classList.add('visually-hidden');
-    window.addEventListener('keydown', modalCloseEsc);
-    overlay.classList.remove('visually-hidden');
-    body.classList.add('isOpenModal');
   }
 
   callbackForm.reset();
