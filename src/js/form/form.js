07822e5e8d25b90ae2def5a8e240c0b6c0ba0ccf
AButsai <butsai@ukr.net>
2022-07-27 18:15:36 +0300

    commit

--- src/js/form/form.js
+++ src/js/form/form.js
@@ -1,29 +1,32 @@
 import { refs } from '../refs/refs.js';
 import { modalCloseEsc } from '../modal/modal.js';
 
-const { callbackForm, callbackWorning, emailInput, overlay, body } = refs;
+const { callbackForm, callbackWorning, emailInput, overlay, body, sendBtn } = refs;
 
 const handleSubmit = e => {
   e.preventDefault();
 
   const formData = new FormData(callbackForm);
-  fetch('/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => {
-      window.addEventListener('keydown', modalCloseEsc);
-      overlay.classList.remove('visually-hidden');
-      body.classList.add('isOpenModal');
-    })
-    .catch(error => alert(error));
 
   const value = emailInput.value;
   if (value.trim() === '') {
     callbackWorning.classList.remove('visually-hidden');
   } else {
     callbackWorning.classList.add('visually-hidden');
+    sendBtn.classList.add('spinner');
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => {
+        window.addEventListener('keydown', modalCloseEsc);
+        overlay.classList.remove('visually-hidden');
+        body.classList.add('isOpenModal');
+        sendBtn.classList.remove('spinner');
+      })
+      .catch(error => alert(error));
   }
 
   callbackForm.reset();
--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -3,8 +3,10 @@ export const refs = {
   overlay: document.querySelector('.overlay'),
   modalClose: document.querySelector('.modal__link'),
   callbackForm: document.querySelector('.callback__form'),
+  sendBtn: document.querySelector('.callback__btn'),
   callbackWorning: document.querySelector('.callback__worning'),
   emailInput: document.querySelector("[name='email']"),
+  loader: document.querySelector('.loader'),
 
   header: document.querySelector('.header'),
 };
